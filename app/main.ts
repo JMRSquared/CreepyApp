63d2a9497f1f11bd86830061ecc7aac7a896ef2b
Joe Sirwali <mulavhe@gmail.com>
2019-08-07 00:33:49 +0200

    Added icon support

--- app/main.ts
+++ app/main.ts
@@ -2,6 +2,19 @@ import Vue from 'nativescript-vue';
 import App from './components/App.vue';
 import store from './store';
 import Notification from './services/notifications';
+import Firebase from './services/firebase';
+
+import {
+  TNSFontIcon,
+  fonticon
+} from "nativescript-fonticon"; // require the couchbase module
+
+TNSFontIcon.debug = false;
+TNSFontIcon.paths = {
+  mdi: "./assets/materialdesignicons.css"
+};
+TNSFontIcon.loadCss();
+Vue.filter("fonticon", fonticon);
 
 // Prints Vue logs when --env.production is *NOT* set while building
 Vue.config.silent = (TNS_ENV === 'production');
@@ -14,6 +27,8 @@ Vue.registerElement(
 // Initialize our main class
 // Everything is done on the constructor soo dont stress
 const notification = new Notification();
+const firebase = new Firebase();
+firebase.init();
 
 Vue.mixin({
   data(){
